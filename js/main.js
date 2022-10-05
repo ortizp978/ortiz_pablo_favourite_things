57e4c169f87ae4d1a843758c119ce2b868ef8ebc
Pablo Ortiz <pablortiz78@MacBook-Pro-de-Pablo.local>
2022-10-05 11:28:52 -0400

    changing photos and code

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
 // imports always go at the top of the file
 //this is called an IIFE (Inmediatly Invoked Function Expression)
-import { person } from "./modules/data.js";
+// import { person } from "./modules/data.js";
+import { getData } from "./modules/dataMiner.js";
 
 //it's a pretty common JavaScript programming pattern
 //also called a module file
 (() => {
-    console.log('fired!');
+    console.log('3 Favourite Things');
 
     let theTeam = document.querySelector('#team-section'),
         theTemplate = document.querySelector('#bio-template').content;
 
-    function loadData() {
+    // Just a test to see if this is imported properly
+    getData();
+
+        //debugger;
+
+    function buildTeam(data) {
         //get all the keys (names) from the data object and use that to iterate through the data
-        const people = Object.keys(person); // Object.keys creates an array
+        
+        //debugger;
+        const people = Object.keys(data); // Object.keys creates an array
 
         people.forEach(prof => {
             // copy the template's contents
@@ -22,15 +30,15 @@ import { person } from "./modules/data.js";
             let containers = panel.firstElementChild.children;
 
             // grab the image from the object and set it as the source
-            containers[0].querySelector('img').src = `images/${person[prof].avatar}`;
+            containers[0].querySelector('img').src = `images/${data[prof].avatar}`;
 
-            containers[1].textContent = person[prof].name;
-            containers[2].textContent = person[prof].role;
-            containers[3].textContent = person[prof].nickname;
+            containers[1].textContent = data[prof].name;
+            containers[2].textContent = data[prof].role;
+            containers[3].textContent = data[prof].nickname;
 
             theTeam.appendChild(panel);
         })
     }
 
-    loadData();
+    getData(buildTeam);
 })();
